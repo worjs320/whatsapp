b597ab92469ceda2893db7f08e961377141a5c5b
worjs320 <worjs320@naver.com>
2020-12-31 00:56:37 +0900

    Sidebar work start.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
-        {/* Sidebar */}
+        <Sidebar />
         {/* Chat */}
       </div>
     </div>
