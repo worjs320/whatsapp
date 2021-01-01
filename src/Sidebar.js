2ee3d2200f0719c2e9d38f3ab91a386d86f70d40
worjs320 <worjs320@naver.com>
2021-01-01 21:09:38 +0900

    Sidebar, avatars api work.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,46 @@
 import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 import "./Sidebar.css";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__header">Sidebar Header</div>
-      <div className="sidebar__search"></div>
-      <div className="sidebar__chats"></div>
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
     </div>
   );
 }
