541c254c73f22caf079bb8e598f30e7c44eba9ea
worjs320 <worjs320@naver.com>
2021-01-03 01:27:46 +0900

    Firebase Databese config, Get firebase database's data.

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./SidebarChat.css";
 import { Avatar } from "@material-ui/core";
 
-function SidebarChat({ addNewChat }) {
+function SidebarChat({ id, name, addNewChat }) {
   const [seed, setSeed] = useState("");
 
   useEffect(() => {
@@ -20,7 +20,7 @@ function SidebarChat({ addNewChat }) {
     <div className="sidebarChat">
       <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
       <div className="sidebarChat__info">
-        <h2>Room name</h2>
+        <h2>{name}</h2>
         <p>Last message...</p>
       </div>
     </div>
