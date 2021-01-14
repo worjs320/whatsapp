a44d8bd234e281468d068f4b9b2567c91ceea6ad
worjs320 <worjs320@naver.com>
2021-01-15 00:40:44 +0900

    Css mobile logic add.

--- src/Login.js
+++ src/Login.js
@@ -21,19 +21,17 @@ function Login() {
   };
 
   return (
-    <div className="login">
-      <div className="login__container">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
-          alt=""
-        />
-        <div className="login__text">
-          <h1>Sign in to WhatsApp</h1>
-        </div>
-        <Button type="submit" onClick={signIn}>
-          Sign In With Google
-        </Button>
+    <div className="login__container">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+        alt=""
+      />
+      <div className="login__text">
+        <h1>Sign in to WhatsApp</h1>
       </div>
+      <Button type="submit" onClick={signIn}>
+        Sign In With Google
+      </Button>
     </div>
   );
 }
